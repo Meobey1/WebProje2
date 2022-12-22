b22776f1024fdcd5bd64d4b93d0b384208234926
Meobey1 <mehmet.oruc@ogr.sakarya.edu.tr>
2022-12-22 18:22:18 +0300

    ASP.NET CORE MVC 6.0 PROJE
    gün 1 =>

---
+++ MVC/B181210399/B181210399/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using B181210399.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace B181210399.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
