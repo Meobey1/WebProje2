e49085b73578c2676942615722ea2e5e86c20719
Meobey1 <mehmet.oruc@ogr.sakarya.edu.tr>
2022-12-23 12:29:04 +0300

    2. Gün
    view,controller düzenlemesi.
    boostrap ekleme.

--- MVC/B181210399/B181210399/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace B181210399.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
