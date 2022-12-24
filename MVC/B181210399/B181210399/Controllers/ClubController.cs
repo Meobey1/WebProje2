057b43e32693a45cde43f8915345c99057c09f92
Meobey1 <mehmet.oruc@ogr.sakarya.edu.tr>
2022-12-24 20:06:07 +0300

    3. gün
    image ekleme.
    düzenlemeler.

---
+++ MVC/B181210399/B181210399/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using B181210399.Data;
+using B181210399.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B181210399.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+    }
+}
---
+++ MVC/B181210399/B181210399/Controllers/RaceController.cs
@@ -0,0 +1,21 @@
+﻿using B181210399.Data;
+using B181210399.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B181210399.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
